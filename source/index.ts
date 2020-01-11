d127fafdda77983c409f3a23188b0eeb93412bad
Alvis HT Tang <alvis@hilbert.space>
2020-01-11 07:46:18 +0000

    init: layout the foundation for the project

---
+++ source/index.ts
@@ -0,0 +1,15 @@
+/*
+ *                           *** CONFIDENTIAL ***
+ * -------------------------------------------------------------------------
+ * Unauthorized copying of this file, via any medium is strictly prohibited.
+ * -------------------------------------------------------------------------
+ *
+ * @summary   Collection of exports
+ *
+ * @author    Alvis HT Tang <alvis@hilbert.space>
+ * @license   Proprietary
+ * @copyright Copyright (c) 2019 - All Rights Reserved.
+ * -------------------------------------------------------------------------
+ */
+
+/* istanbul ignore next */
