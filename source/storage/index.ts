3626674a6f3f947148d0c0fb26b129feb0fdb59f
Alvis HT Tang <alvis@hilbert.space>
2020-03-25 12:21:45 +0000

    arch(sto): add the standard interface for storage backends

--- source/storage/index.ts
+++ source/storage/index.ts
@@ -5,7 +5,7 @@
  * See the LICENSE file for details.
  * -------------------------------------------------------------------------
  *
- * @summary   Collection of exports
+ * @summary   Collection of storage adapters
  *
  * @author    Alvis HT Tang <alvis@hilbert.space>
  * @license   MIT
@@ -15,9 +15,4 @@
 
 /* istanbul ignore file */
 
-export * from './storage';
-
-export * from './schema';
-export * from './types';
-
-export {};
+export * from './prototype';
